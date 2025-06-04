9bc3e462b416ce2b4c1aafdedade81cf70a8f426
Sam <samtmartey@gmail.com>
2025-06-04 14:45:33 +0000

    fix: update database connection logic to use environment-specific database names

--- db/index.js
+++ db/index.js
@@ -4,8 +4,12 @@ import { development } from "../config/constants.js";
 const DEV_DB = process.env.DEV_DB;
 const PROD_DB = process.env.PROD_DB;
 const NODE_ENV = process.env.NODE_ENV;
+const PROD_DB_NAME = process.env.DB_NAME;
+const DEV_DB_NAME = process.env.DEV_DB_NAME;
 
 const DB_URI = NODE_ENV === development ? DEV_DB : PROD_DB;
+const DBNAME = NODE_ENV === development ? DEV_DB_NAME : PROD_DB_NAME;
+
 const dbConn = mongoose.connection;
 dbConn.on("connected", () => {
   console.log("Mongoose connected");
@@ -13,7 +17,8 @@ dbConn.on("connected", () => {
 
 const connectDB = () => {
   mongoose.connect(DB_URI, {
-    autoIndex: false,
+    dbName: DBNAME,
+    autoIndex: true,
   });
 };
 

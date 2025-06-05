8fb71513677dd3faec0c4a81262961fb59635ccd
Sam <samtmartey@gmail.com>
2025-06-05 12:19:56 +0000

    fix: remove unused NODE_ENV variable and ensure database name is logged on connection

--- db/index.js
+++ db/index.js
@@ -3,7 +3,6 @@ import { isDevelopment } from "../config/constants.js";
 
 const DEV_DB = process.env.DEV_DB;
 const PROD_DB = process.env.PROD_DB;
-const NODE_ENV = process.env.NODE_ENV;
 const PROD_DB_NAME = process.env.DB_NAME;
 const DEV_DB_NAME = process.env.DEV_DB_NAME;
 
@@ -13,6 +12,7 @@ const DBNAME = isDevelopment ? DEV_DB_NAME : PROD_DB_NAME;
 const dbConn = mongoose.connection;
 dbConn.on("connected", () => {
   console.log("Mongoose connected");
+  console.log(`Database: ${DBNAME}`);
 });
 
 const connectDB = () => {

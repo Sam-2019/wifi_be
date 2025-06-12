5b00a876838b0d9ccf8ce6fe2a3e3949370b36e5
Sam <samtmartey@gmail.com>
2025-06-12 16:20:09 +0000

    refactor: streamline database connection logic and integrate seed functionality for development

--- db/index.js
+++ db/index.js
@@ -1,22 +1,16 @@
 import mongoose from "mongoose";
-import { isDevelopment } from "../config/constants.js";
-
-const DEV_DB = process.env.DEV_DB;
-const PROD_DB = process.env.PROD_DB;
-const PROD_DB_NAME = process.env.PROD_DB_NAME;
-const DEV_DB_NAME = process.env.DEV_DB_NAME;
-
-const DB_URI = isDevelopment ? DEV_DB : PROD_DB;
-const DBNAME = isDevelopment ? DEV_DB_NAME : PROD_DB_NAME;
+import { seed } from "../config/seed.js";
+import { db_uri, db_name, isDevelopment } from "../config/constants.js";
 
 const dbConn = mongoose.connection;
 dbConn.on("connected", () => {
+  if (isDevelopment) seed();
   console.log("Mongoose connected");
 });
 
 const connectDB = () => {
-  mongoose.connect(DB_URI, {
-    dbName: DBNAME,
+  mongoose.connect(db_uri, {
+    dbName: db_name,
     autoIndex: false,
   });
 };

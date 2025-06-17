d606ac5afece665b97e5383ea7c6efd2a915e6d8
Sam <samtmartey@gmail.com>
2025-06-17 10:55:32 +0000

    added logger

--- admin/index.js
+++ admin/index.js
@@ -1,20 +1,21 @@
 import bcrypt from "bcrypt";
-import {
-  SmsResource,
-  UserResource,
-  SaleResource,
-  RegistrationResource,
-  FailedRegistrationResource,
-  PendingRegistrationResource,
-} from "../admin/resources.js";
 import { dbSession } from "../db/index.js";
+import loggerFeature from '@adminjs/logger';
 import AdminJSExpress from "@adminjs/express";
 import { dashboard } from "../config/filePath.js";
 import AdminJS, { ComponentLoader } from "adminjs";
 import { findUser } from "../db/repository/user.js";
 import * as AdminJSMongoose from "@adminjs/mongoose";
 import { cookie, cookiePass } from "../config/constants.js";
-import { adminCredentials, admin, companyName } from "../config/constants.js";
+import { adminCredentials, admin, companyName, salt } from "../config/constants.js";
+
+import Sms from "../db/modelsXschema/sms.js";
+import Sale from "../db/modelsXschema/sale.js";
+import User from "../db/modelsXschema/user.js";
+import Registration from "../db/modelsXschema/registration.js";
+import FailedRegistration from "../db/modelsXschema/failed_registration.js";
+import PendingRegistration from "../db/modelsXschema/pending_registration.js";
+import Logger from "../db/modelsXschema/log.js";
 
 // const authenticate = async (email, password) => {
 //   if (
@@ -37,6 +38,10 @@ const authenticate = async (email, password) => {
   return false;
 };
 
+const isAdminRole = ({ currentAdmin }) => {
+  return currentAdmin && currentAdmin.role === admin;
+};
+
 AdminJS.registerAdapter({
   Resource: AdminJSMongoose.Resource,
   Database: AdminJSMongoose.Database,
@@ -47,9 +52,290 @@ const Components = {
   Dashboard: componentLoader.add("Dashboard", dashboard),
 };
 
-const isAdminRole = ({ currentAdmin }) => {
-  return currentAdmin && currentAdmin.role === admin;
+const logger = loggerFeature({
+  componentLoader,
+  propertiesMapping: {
+    user: 'userId',
+  },
+  userIdAttribute: 'id',
+});
+
+const RegistrationResource = {
+  resource: Registration,
+  features: [
+    logger
+  ],
+  options: {
+    id: "registrations",
+    listProperties: ["regID", "fullName", "dateTime"],
+    filterProperties: ["regID", "fullName", "dateTime"],
+    editProperties: ["regID", "fullName", "dateTime"],
+    showProperties: ["regID", "fullName", "dateTime"],
+    sort: {
+      sortBy: "updatedAt",
+      direction: "desc",
+    },
+    actions: {
+      edit: {
+        isAccessible: false,
+        isVisible: true,
+      },
+      delete: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+      },
+      bulkDelete: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+      },
+    },
+  },
 };
+
+const PendingRegistrationResource = {
+  resource: PendingRegistration,
+  features: [
+    logger
+  ],
+  options: {
+    id: "pending_registrations",
+    listProperties: ["regID", "fullName", "dateTime"],
+    filterProperties: ["regID", "fullName", "dateTime"],
+    editProperties: ["regID", "fullName", "dateTime"],
+    showProperties: ["regID", "fullName", "dateTime"],
+    sort: {
+      sortBy: "updatedAt",
+      direction: "desc",
+    },
+    actions: {
+      edit: {
+        isAccessible: false,
+        isVisible: true,
+      },
+      delete: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+      },
+      bulkDelete: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+      },
+    },
+  },
+};
+
+const SaleResource = {
+  resource: Sale,
+  features: [
+    logger
+  ],
+  options: {
+    id: "sales",
+    listProperties: ["regID", "fullName", "dateTime"],
+    filterProperties: ["regID", "fullName", "dateTime"],
+    editProperties: ["regID", "fullName", "dateTime"],
+    showProperties: ["regID", "fullName", "dateTime"],
+    sort: {
+      sortBy: "updatedAt",
+      direction: "desc",
+    },
+    actions: {
+      edit: {
+        isAccessible: false,
+        isVisible: true,
+      },
+      delete: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+      },
+      bulkDelete: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+      },
+    },
+  },
+};
+
+const FailedRegistrationResource = {
+  resource: FailedRegistration,
+  features: [
+    logger
+  ],
+  options: {
+    id: "failed_registrations",
+    listProperties: ["regID", "fullName", "dateTime"],
+    filterProperties: ["regID", "fullName", "dateTime"],
+    editProperties: ["regID", "fullName", "dateTime"],
+    showProperties: ["regID", "fullName", "dateTime"],
+    sort: {
+      sortBy: "updatedAt",
+      direction: "desc",
+    },
+    actions: {
+      edit: {
+        isAccessible: false,
+        isVisible: true,
+      },
+      delete: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+      },
+      bulkDelete: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+      },
+    },
+  },
+};
+
+const SmsResource = {
+  resource: Sms,
+  features: [
+    logger
+  ],
+  options: {
+    id: "sms_receipts",
+    listProperties: ["mobileNumber", "message", "provider", "payload"],
+    filterProperties: ["mobileNumber", "message", "provider", "payload"],
+    editProperties: ["mobileNumber", "message", "provider", "payload"],
+    showProperties: ["mobileNumber", "message", "provider", "payload"],
+    sort: {
+      sortBy: "updatedAt",
+      direction: "desc",
+    },
+    actions: {
+      edit: {
+        isAccessible: false,
+        isVisible: true,
+      },
+      delete: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+      },
+      bulkDelete: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+      },
+    },
+  },
+};
+
+const UserResource = {
+  resource: User,
+  features: [
+    logger
+  ],
+  options: {
+    id: "users",
+    properties: {
+      encryptedPassword: {
+        isVisible: false,
+      },
+      password: {
+        type: "string",
+        isVisible: {
+          list: false,
+          edit: true,
+          filter: false,
+          show: false,
+        },
+      },
+    },
+    actions: {
+      new: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+        before: async (request) => {
+          if (request.payload?.password) {
+            request.payload = {
+              ...request.payload,
+              encryptedPassword: await bcrypt.hash(
+                request.payload.password,
+                salt
+              ),
+              password: undefined,
+            };
+          }
+          return request;
+        },
+      },
+      show: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+        after: async (response) => {
+          response.record.params.password = "";
+          return response;
+        },
+      },
+      edit: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+        before: async (request) => {
+          if (request.method === "post") {
+            if (request.payload?.password) {
+              request.payload = {
+                ...request.payload,
+                encryptedPassword: await bcrypt.hash(
+                  request.payload.password,
+                  salt
+                ),
+                password: undefined,
+              };
+            } else {
+              // biome-ignore lint/performance/noDelete: <explanation>
+              delete request.payload?.password;
+            }
+          }
+          return request;
+        },
+        after: async (response) => {
+          response.record.params.password = "";
+          return response;
+        },
+      },
+      list: {
+        after: async (response) => {
+          for (const record of response.records) {
+            record.params.password = "";
+          }
+          return response;
+        },
+      },
+      delete: {
+        isAccessible: ({ currentAdmin }) => currentAdmin.role === admin,
+      },
+      bulkDelete: {
+        isAccessible: ({ currentAdmin }) => isAdminRole({ currentAdmin }),
+      },
+    },
+  },
+};
+
+const LogResource = {
+  resource: Logger,
+  options: {
+    id: "logs",
+  },
+  featureOptions: {
+    propertiesMapping: {
+      recordTitle: 'title',
+      userIdAttribute: 'id',
+      resourceOptions: {
+        navigation: {
+          name: 'SectionName',
+          icon: 'iconName'
+        }
+      }
+    }
+  },
+  actions: {
+    new: {
+      isAccessible: false,
+    },
+    show: {
+      isAccessible: false
+    },
+    edit: {
+      isAccessible: false
+    },
+    list: {
+      isAccessible: false
+    },
+    delete: {
+      isAccessible: false,
+    },
+    bulkDelete: {
+      isAccessible: false,
+    },
+  },
+}
 
 const adminOptions = {
   branding: {
@@ -62,6 +348,7 @@ const adminOptions = {
   },
   componentLoader,
   resources: [
+    LogResource,
     SmsResource,
     UserResource,
     SaleResource,
@@ -131,4 +418,4 @@ const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
   dbSession
 );
 
-export { adminjs, adminRouter, authenticate, isAdminRole };
+export { adminjs, adminRouter };

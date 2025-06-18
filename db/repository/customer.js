180358087772a8c0732102323953cb92b3dbd2ba
Sam <samtmartey@gmail.com>
2025-06-18 11:52:42 +0000

    feat: add customer model and repository for customer management

---
+++ db/repository/customer.js
@@ -0,0 +1,28 @@
+import Customer from "../modelsXschema/customer.js";
+import { excludeItems } from "../../config/constants.js";
+
+const getCustomers = async () => {
+    return await Customer.find({}, excludeItems).lean();
+};
+
+const addCustomer = async (data) => {
+    return await Customer.create(data);
+};
+
+const getCustomer = async (data) => {
+    const email = data?.email;
+    const userName = data?.userName;
+    const phoneNumber = data?.phoneNumber;
+
+    return await Customer.findOne(
+        {
+            "credentials.userName": userName,
+            phoneNumber: phoneNumber,
+            email: email,
+            registrationType: registration
+        },
+        excludeItems
+    ).lean();
+};
+
+export { getCustomers, addCustomer, getCustomer };

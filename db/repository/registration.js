dead46b1caa92fb2d55d9cb1d598d4483d9f7557
Sam-2019 <samtmartey@gmail.com>
2025-06-18 08:33:23 +0000

    updated registration repository

--- db/repository/registration.js
+++ db/repository/registration.js
@@ -1,5 +1,5 @@
 import Registration from "../modelsXschema/registration.js";
-import { excludeItems } from "../../config/constants.js";
+import { excludeItems, registration, topup } from "../../config/constants.js";
 
 const getRegistrations = async () => {
   return await Registration.find({}, excludeItems).lean();
@@ -10,14 +10,17 @@ const addRegistration = async (data) => {
 };
 
 const findRegistration = async (data) => {
-  const phoneNumber = data.phoneNumber;
-  const email = data.email;
+  const email = data?.email;
+  const userName = data?.userName;
+  const phoneNumber = data?.phoneNumber;
   // const clientReference = data.clientReference;
 
   return await Registration.findOne(
     {
+      "credentials.userName": userName,
       phoneNumber: phoneNumber,
       email: email,
+      registrationType: registration,
       // clientReference: clientReference,
     },
     excludeItems

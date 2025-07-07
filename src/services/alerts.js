f1298b2713d60d2355661fe762414c693628215b
Sam <samtmartey@gmail.com>
2025-07-07 18:01:23 +0000

    refactor: update alert functions to accept payload and improve alert setup

--- src/services/alerts.js
+++ src/services/alerts.js
@@ -1,60 +1,55 @@
 import { ntfyUri, ntfyTopic, ntfyAuthorization } from "../config/constants.js";
 
 const priority = 4;
-const tags = ["registered"];
 const topic = ntfyTopic;
+// const tags = ["warning", "cd"];
 
 const setupAlert = (title, message) => {
   return {
-    topic: topic,
-    message: message,
-    title: title,
-    tags: tags,
-    priority: priority,
+    "topic": topic,
+    "title": title,
+    // "tags": tags,
+    "priority": priority,
+    "message": JSON.stringify(message),
   };
 }
 
-const saleAlert = () => {
-  return setupAlert("New Sale", `${payload}`);
+
+const saleAlert = (payload) => {
+  return setupAlert("New Sale", payload);
 }
 
-const registrationAlert = () => {
-  return setupAlert("New Registration", `${payload}`);
+const registrationAlert = (payload) => {
+  return setupAlert("Registration", payload);
 }
 
-const pendingAlert = () => {
-  return setupAlert("Pending Registration", `${payload}`);
+const pendingAlert = (payload) => {
+  return setupAlert("Pending Registration", payload);
 }
 
-
-const failedAlert = () => {
-  return setupAlert("Failed Registration", `${payload}`);
+const failedAlert = (payload) => {
+  return setupAlert("Failed Registration", payload);
 }
 
-const customerAlert = () => {
-  return setupAlert("New Customer", `${payload}`);
+const customerAlert = (payload) => {
+  return setupAlert("New Customer", payload);
 }
 
-const callbackAlert = () => {
-  return setupAlert("Payment Callback", `${payload}`);
+const callbackAlert = (payload) => {
+  return setupAlert("Payment Callback", payload);
 }
 
-const statusCheckAlert = () => {
-  return setupAlert("Payment Status Check", `${payload}`);
+const statusCheckAlert = (payload) => {
+  return setupAlert("Payment Status Check", payload);
 }
 
-const syncAlert = () => {
-  return setupAlert("Data Sync", `${payload}`);
+const syncAlert = (payload) => {
+  return setupAlert("Data Sync", payload);
 }
 
-export const ntfy = async (payload, route) => {
+export const ntfy = async ({ payload, route }) => {
   if (!ntfyUri || !ntfyTopic || !ntfyAuthorization) {
     console.error("Ntfy configuration is missing.");
-    return;
-  }
-
-  if (!payload) {
-    console.error("Payload is required for ntfy notification.");
     return;
   }
 
@@ -63,38 +58,52 @@ export const ntfy = async (payload, route) => {
     return;
   }
 
+  if (!payload) {
+    console.error("Payload is required for ntfy notification.");
+    return;
+  }
+
+  const getInfo = {
+    name: payload?.fullName,
+    blockCourt: payload?.blockCourt,
+    roomNumber: payload?.roomNumber,
+    subscriptionPlan: payload?.subscriptionPlan,
+    clientReference: payload?.clientReference,
+    userName: payload?.credentials?.username,
+    registrationType: payload?.registrationType
+  }
+
+
   const getPayload = (route) => {
     switch (route) {
       case 'Sale':
-        return saleAlert();
-      case 'Registration':
-        return registrationAlert();
-      case 'Pending Registration':
-        return pendingAlert();
-      case 'Failed Registration':
-        return failedAlert();
-      case 'Customer':
-        return customerAlert();
-      case 'Payment Callback':
-        return callbackAlert();
-      case 'Payment Status':
-        return statusCheckAlert();
+        return saleAlert(getInfo);
       case 'Data Sync':
-        return syncAlert();
+        return syncAlert(getInfo);
+      case 'Failed Registration':
+        return failedAlert(getInfo);
+      case 'Pending Registration':
+        return pendingAlert(getInfo);
+      case 'Customer':
+        return customerAlert(getInfo);
+      case 'Payment Callback':
+        return callbackAlert(getInfo);
+      case 'Payment Status':
+        return statusCheckAlert(getInfo);
+      case 'Registration':
+        return registrationAlert(getInfo);
       default:
         console.error("Invalid alert type provided.");
         return null;
     }
   };
 
-  fetch(ntfyUri, {
+  await fetch(ntfyUri, {
     method: "POST",
-    mode: "no-cors",
     headers: {
       'Authorization': `Bearer ${ntfyAuthorization}`
     },
-    body: JSON.stringify(getPayload(payload)),
-  }).then(() => {
-    // console.log("Data sent to Google Scripts successfully");
-  });
+    body: JSON.stringify(getPayload(route)),
+  })
+
 };

daf4c100ee636cdc57ab23cc6a677cf9f1f2801e
Sam <samtmartey@gmail.com>
2025-07-09 22:59:18 +0000

    feat: implement job scheduling with Bree for account provisioning

---
+++ src/services/jobs/index.js
@@ -0,0 +1,34 @@
+import Bree from "bree";
+import path from "path";
+import Cabin from "cabin";
+import Graceful from "@ladjs/graceful";
+import Signale from "signale/signale.js";
+import { disconnectDB } from "../db";
+
+// initialize cabin
+const cabin = new Cabin({
+    axe: {
+        logger: new Signale()
+    }
+});
+
+const bree = new Bree({
+    logger: cabin,
+    root: path.resolve("./src/services/jobs"),
+    jobs: [
+        // runs `./jobs/worker-1.js` after 1 minute and every 1 minutes thereafter
+        {
+            name: "provisionAccount",
+            timeout: "1m",
+            interval: "1m",
+        },
+    ],
+});
+
+const graceful = new Graceful({
+    brees: [bree],
+    mongooses: [disconnectDB]
+});
+graceful.listen();
+
+export { bree };

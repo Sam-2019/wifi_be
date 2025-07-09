369a3fdc029f021af82e1d3c53fab376eed4fdb3
Sam <samtmartey@gmail.com>
2025-07-09 22:59:41 +0000

    chore: clean up code structure and improve readability

--- api/index.js
+++ api/index.js
@@ -6,8 +6,20 @@ import router from "../src/routes/index.js";
 import { connectDB } from "../src/services/db/index.js";
 import { __dirname } from "../src/config/constants.js";
 import { adminjs, adminRouter } from "../src/services/admin/index.js";
-
+import { bree } from "../src/services/jobs/index.js";
+import Cabin from "cabin";
+import Signale from "signale/signale.js";
+import requestId from 'express-request-id';
+import requestReceived from 'request-received';
+import responseTime from 'response-time';
 const port = process.env.PORT || 4000;
+
+// initialize cabin
+const cabin = new Cabin({
+  axe: {
+    logger: new Signale()
+  }
+});
 
 const start = async () => {
   const app = express();
@@ -17,9 +29,13 @@ const start = async () => {
       methods: ["GET", "POST"],
     }),
   );
-  connectDB();
-
+  await connectDB();
   app.use(json());
+
+  app.use(requestReceived);
+  app.use(responseTime());
+  app.use(requestId());
+  app.use(cabin.middleware);
   app.use("/", router);
   app.use(bodyParser.json());
   app.disable("x-powered-by");
@@ -31,6 +47,7 @@ const start = async () => {
     console.log(`Server is running on port ${port}`);
     console.log(`AdminJS started on ${port}${adminjs.options.rootPath}`);
   });
+  await bree.start();
 };
 
 start();

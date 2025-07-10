3c2dbbac4bea7129628fed39a1fb2b4a234c50d4
Sam <samtmartey@gmail.com>
2025-07-10 17:43:48 +0000

    added ntfy

--- src/services/alerts/ntfy.js
+++ src/services/alerts/ntfy.js
@@ -5,66 +5,64 @@ const topic = ntfyTopic;
 // const tags = ["warning", "cd"];
 
 const setupAlert = (title, message) => {
-    return {
-        "topic": topic,
-        "title": title,
-        // "tags": tags,
-        "priority": priority,
-        "message": JSON.stringify(message),
-    };
-}
+  return {
+    topic: topic,
+    title: title,
+    // "tags": tags,
+    priority: priority,
+    message: JSON.stringify(message),
+  };
+};
 
 const successAlert = (payload) => {
-    return setupAlert("Account Provisioned", payload);
-}
+  return setupAlert("Account Provisioned", payload);
+};
 
 const failAlert = (payload) => {
-    return setupAlert("Account Provisioned", payload);
-}
+  return setupAlert("Account Provisioned", payload);
+};
 
 export const ntfy = async ({ payload, route }) => {
-    if (!ntfyUri || !ntfyTopic || !ntfyAuthorization) {
-        console.error("Ntfy configuration is missing.");
-        return;
+  if (!ntfyUri || !ntfyTopic || !ntfyAuthorization) {
+    console.error("Ntfy configuration is missing.");
+    return;
+  }
+  if (!route) {
+    console.error("Route is required for ntfy notification.");
+    return;
+  }
+
+  if (!payload) {
+    console.error("Payload is required for ntfy notification.");
+    return;
+  }
+
+  const getInfo = {
+    name: payload?.fullName,
+    blockCourt: payload?.blockCourt,
+    roomNumber: payload?.roomNumber,
+    clientReference: payload?.clientReference,
+    userName: payload?.credentials?.username,
+    registrationType: payload?.registrationType,
+  };
+
+  const getPayload = (route) => {
+    switch (route) {
+      case "/provisionSuccess":
+        return successAlert(getInfo);
+      case "/provisionFailed":
+        return failAlert(payload);
+      default:
+        console.error("Invalid alert type provided.");
+        return null;
     }
-    if (!route) {
-        console.error("Route is required for ntfy notification.");
-        return;
-    }
+  };
 
-    if (!payload) {
-        console.error("Payload is required for ntfy notification.");
-        return;
-    }
-
-    const getInfo = {
-        name: payload?.fullName,
-        blockCourt: payload?.blockCourt,
-        roomNumber: payload?.roomNumber,
-        clientReference: payload?.clientReference,
-        userName: payload?.credentials?.username,
-        registrationType: payload?.registrationType
-    }
-
-
-    const getPayload = (route) => {
-        switch (route) {
-            case '/provisionSuccess':
-                return successAlert(getInfo);
-            case '/provisionFailed':
-                return failAlert(getInfo);
-            default:
-                console.error("Invalid alert type provided.");
-                return null;
-        }
-    };
-
-    await fetch(ntfyUri, {
-        method: "POST",
-        headers: {
-            'Authorization': `Bearer ${ntfyAuthorization}`
-        },
-        body: JSON.stringify(getPayload(route)),
-    })
-
+  await fetch(ntfyUri, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${ntfyAuthorization}`,
+    },
+    body: JSON.stringify(getPayload(route)),
+  });
 };
--- src/services/jobs/provisionAccount.js
+++ src/services/jobs/provisionAccount.js
@@ -2,31 +2,32 @@ import { connectDB } from "../db";
 import { createUser } from "../mikrotik/index.js";
 import { todaySales } from "../db/repository/sale.js";
 import { getSelectedPlan } from "../../config/constants.js";
+import { ntfy } from "../alerts/ntfy.js";
 
 const setupUser = async () => {
-    await connectDB();
-    const paidRegistrants = await todaySales();
-    if (paidRegistrants.length > 0) {
-        console.log(`Today's sales: ${paidRegistrants.length}`);
-        paidRegistrants.forEach(user => {
-            console.log(`User: ${user.credentials.userName}, Email: ${user.email}`);
-            const profile = getSelectedPlan(user.subscriptionPlan);
-            const results = {
-                ...user,
-                profile: profile
-            }
-            createUser(results)
-                .then(async response => {
-                    await ntfy({ route: "/provisionSuccess", payload: results });
-                })
-                .catch(async error => {
-                    await ntfy({ route: "/provisionFailed", payload: results });
-                    console.error(`Error adding user ${user.credentials.userName}:`, error);
-                });
+  await connectDB();
+  const paidRegistrants = await todaySales();
+  if (paidRegistrants.length > 0) {
+    console.log(`Today's sales: ${paidRegistrants.length}`);
+    paidRegistrants.forEach((user) => {
+      console.log(`User: ${user.credentials.userName}, Email: ${user.email}`);
+      const profile = getSelectedPlan(user.subscriptionPlan);
+      const results = {
+        ...user,
+        profile: profile,
+      };
+      createUser(results)
+        .then(async (response) => {
+          await ntfy({ route: "/provisionSuccess", payload: results });
+        })
+        .catch(async (error) => {
+          await ntfy({ route: "/provisionFailed", payload: error });
+          // console.error(`Error adding user ${user.credentials.userName}:`, error);
         });
-    } else {
-        console.log("No sales found for today.");
-    }
+    });
+  } else {
+    console.log("No sales found for today.");
+  }
 };
 
 setupUser();

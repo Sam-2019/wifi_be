60b274bfe73cee830b492cb42f730a96a8030c87
Sam <samtmartey@gmail.com>
2025-07-11 14:27:24 +0000

    added import

--- src/config/utils.js
+++ src/config/utils.js
@@ -1,39 +1,39 @@
-import { authToken, hubtel } from "./constants";
+import { authToken, hubtel, apiUrl } from "./constants";
 
 export const getFetchOption = {
-    method: "GET",
-    headers: {
-        Authorization: `Bearer ${authToken}`,
-        "Content-Type": "application/json",
-    },
+	method: "GET",
+	headers: {
+		Authorization: `Bearer ${authToken}`,
+		"Content-Type": "application/json",
+	},
 };
 
 export const fetchRequest = async (results, options = getFetchOption) => {
-    const queryParams = {
-        clientReference: results.clientReference,
-        transactionId: results.transactionId,
-        externalTransactionId: results.externalTransactionId,
-    };
+	const queryParams = {
+		clientReference: results.clientReference,
+		transactionId: results.transactionId,
+		externalTransactionId: results.externalTransactionId,
+	};
 
-    const queryString = new URLSearchParams(queryParams).toString();
-    const endpoint = `${apiUrl}?${queryString}`;
+	const queryString = new URLSearchParams(queryParams).toString();
+	const endpoint = `${apiUrl}?${queryString}`;
 
-    try {
-        const response = await fetch(endpoint, options);
-        return response;
-    } catch (error) {
-        console.error("Fetch error:", error);
-        throw error;
-    }
+	try {
+		const response = await fetch(endpoint, options);
+		return response;
+	} catch (error) {
+		console.error("Fetch error:", error);
+		throw error;
+	}
 };
 
 export const modifiedSalesRecord = (registrationByRef, responseData) => {
-    const dataPayload = responseData?.data;
-    return {
-        ...registrationByRef,
-        provider: hubtel.toUpperCase(),
-        providerResponse: responseData,
-        transactionId: dataPayload?.transactionId,
-        externalTransactionId: dataPayload?.externalTransactionId,
-    };
-}
+	const dataPayload = responseData?.data;
+	return {
+		...registrationByRef,
+		provider: hubtel.toUpperCase(),
+		providerResponse: responseData,
+		transactionId: dataPayload?.transactionId,
+		externalTransactionId: dataPayload?.externalTransactionId,
+	};
+};

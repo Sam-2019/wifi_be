82f9253726f298dcfa3b6275f7a2f303861c32dc
Sam <samtmartey@gmail.com>
2025-07-11 14:24:15 +0000

    whitespace

--- src/services/db/modelsXschema/customer.js
+++ src/services/db/modelsXschema/customer.js
@@ -1,34 +1,34 @@
 import { model, Schema } from "mongoose";
 
 const dataSchema = new Schema(
-  {
-    regID: { type: String },
-    fullName: { type: String },
-    phoneNumber: { type: String },
-    email: { type: String, lowercase: true, unique: true },
-    dateOfBirth: { type: Date },
-    blockCourt: { type: String },
-    roomType: { type: String },
-    roomNumber: { type: String },
-    isCustodian: { type: Boolean, default: false },
-    dateTime: { type: Date, default: Date.now() },
-    credentials: {
-      userName: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-    },
-    cardPrinted: { type: Boolean, default: false },
-       profileCreated: { type: Boolean, default: false },
-  },
-  {
-    timestamps: true,
-  },
+	{
+		regID: { type: String },
+		fullName: { type: String },
+		phoneNumber: { type: String },
+		email: { type: String, lowercase: true, unique: true },
+		dateOfBirth: { type: Date },
+		blockCourt: { type: String },
+		roomType: { type: String },
+		roomNumber: { type: String },
+		isCustodian: { type: Boolean, default: false },
+		dateTime: { type: Date, default: Date.now() },
+		credentials: {
+			userName: {
+				type: String,
+				required: true,
+				unique: true,
+			},
+			password: {
+				type: String,
+				required: true,
+			},
+		},
+		cardPrinted: { type: Boolean, default: false },
+		profileCreated: { type: Boolean, default: false },
+	},
+	{
+		timestamps: true,
+	},
 );
 
 const Customer = model("Customer", dataSchema);

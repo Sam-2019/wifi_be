ce65a4d84ab5629f749252d3b3e24808df3a9764
Sam <samtmartey@gmail.com>
2025-07-11 15:28:02 +0000

    req, res timer

--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ import path from "path";
 import cors from "cors";
 import bodyParser from "body-parser";
 import express, { json } from "express";
+import responseTime from "response-time";
+import requestId from "express-request-id";
 import router from "../src/routes/index.js";
+import requestReceived from "request-received";
 import { ping } from "../src/services/pinger.js";
 import { __dirname } from "../src/config/constants.js";
 import { connectDB } from "../src/services/db/index.js";
@@ -10,27 +13,30 @@ import { connectDB } from "../src/services/db/index.js";
 const port = process.env.PORT || 4000;
 
 const start = async () => {
-  const app = express();
-  app.use(
-    cors({
-      origin: "*",
-      methods: ["GET", "POST"],
-    }),
-  );
+	const app = express();
+	app.use(
+		cors({
+			origin: "*",
+			methods: ["GET", "POST"],
+		}),
+	);
 
-  connectDB();
+	await connectDB();
 
-  ping();
-  app.use(json());
-  app.use("/", router);
-  app.use(bodyParser.json());
-  app.disable("x-powered-by");
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(express.static(path.join(__dirname, "/public")));
+	ping();
+	app.use(json());
+	app.use(requestId());
+	app.use("/", router);
+	app.use(responseTime());
+	app.use(requestReceived);
+	app.use(bodyParser.json());
+	app.disable("x-powered-by");
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(express.static(path.join(__dirname, "/public")));
 
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+	app.listen(port, () => {
+		console.log(`Server is running on port ${port}`);
+	});
 };
 
 start();

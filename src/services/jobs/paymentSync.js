adf21378ad32a2ead4f71a86b0c5686c0fd6e8b1
Sam <samtmartey@gmail.com>
2025-07-14 21:59:19 +0000

    paymentSync

---
+++ src/services/jobs/paymentSync.js
@@ -0,0 +1,42 @@
+import { cabin } from "./index.js";
+import { connectDB } from "../db/index.js";
+import { findSale } from "../db/repository/sale.js";
+import { bearer, baseURL } from "../../config/constants.js";
+import { getTodaysRegistrations } from "../db/repository/registration.js";
+
+const paymentSync = async () => {
+  await connectDB();
+  const registrations = await getTodaysRegistrations();
+  if (registrations.length > 0) {
+    registrations.forEach(async (user) => {
+      const clientReference = user?.clientReference;
+      const payload = {
+        clientReference: clientReference,
+      };
+
+      const sale = await findSale(clientReference)
+      if (sale.clientReference) return;
+
+      await fetch(`${baseURL}/api/payment/sync`, {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify(payload),
+        headers: {
+          Authorization: `Bearer ${bearer}`,
+          "Content-Type": "application/json",
+        },
+      }).then(async (response) => {
+        if (response.status === 200) {
+          return await ntfy({ route: "/paymentSync", payload: response });
+        }
+        await ntfy({ route: "/paymentSync", payload: "Registration not found" });
+      }).catch(async (err) => {
+        await ntfy({ route: "/paymentSync", payload: err });
+      })
+    });
+  } else {
+    cabin.info("All sales synced");
+  }
+};
+
+paymentSync();

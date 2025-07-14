33c07e79c941d86948e033facc4b04bbdb086c3b
Sam <samtmartey@gmail.com>
2025-07-14 15:50:47 +0000

    add customer

--- src/routes/sale.js
+++ src/routes/sale.js
@@ -3,66 +3,67 @@ import express from "express";
 import { ntfy } from "../services/alerts.js";
 import { writeToSheet } from "../services/gSheet.js";
 import { authMiddleware } from "../config/middleware.js";
-import { internalServerError } from "../config/constants.js";
+import { internalServerError, registration } from "../config/constants.js";
 import { addSale, findSale, getSales } from "../services/db/repository/sale.js";
 
 const router = express.Router();
 router.get("/sales", authMiddleware, async (req, res) => {
-	try {
-		const sales = await getSales();
-		if (!sales || sales.length === 0) {
-			return res.status(404).json({ message: "No sales found", data: [] });
-		}
-		res.status(200).json({ message: sales });
-	} catch (error) {
-		console.error("Error in /sales:", error);
-		res.status(500).send(internalServerError);
-	}
+  try {
+    const sales = await getSales();
+    if (!sales || sales.length === 0) {
+      return res.status(404).json({ message: "No sales found", data: [] });
+    }
+    res.status(200).json({ message: sales });
+  } catch (error) {
+    console.error("Error in /sales:", error);
+    res.status(500).send(internalServerError);
+  }
 });
 
 router
-	.route("/sale")
-	.get(authMiddleware, async (req, res) => {
-		const results = req.query;
+  .route("/sale")
+  .get(authMiddleware, async (req, res) => {
+    const results = req.query;
 
-		if (results === undefined || results === null) {
-			console.error("Received with no data");
-			return res.status(400).send("Received with no data");
-		}
+    if (results === undefined || results === null) {
+      console.error("Received with no data");
+      return res.status(400).send("Received with no data");
+    }
 
-		try {
-			const sale = await findSale(results);
-			if (!sale || sale.length === 0) {
-				return res.status(404).json({ message: "No sale found", data: null });
-			}
-			res.status(200).json({ message: "Sale found", data: sale });
-		} catch (error) {
-			console.error("Error in /Sale:", error);
-			res.status(500).send(internalServerError);
-		}
-	})
-	.post(authMiddleware, async (req, res) => {
-		const results = req.body;
+    try {
+      const sale = await findSale(results);
+      if (!sale || sale.length === 0) {
+        return res.status(404).json({ message: "No sale found", data: null });
+      }
+      res.status(200).json({ message: "Sale found", data: sale });
+    } catch (error) {
+      console.error("Error in /Sale:", error);
+      res.status(500).send(internalServerError);
+    }
+  })
+  .post(authMiddleware, async (req, res) => {
+    const results = req.body;
 
-		if (
-			results === undefined ||
-			results === null ||
-			results.clientReference === undefined ||
-			results.clientReference === null
-		) {
-			console.error("Registration received with no data");
-			return res.status(400).send("Registration received with no data");
-		}
+    if (
+      results === undefined ||
+      results === null ||
+      results.clientReference === undefined ||
+      results.clientReference === null
+    ) {
+      console.error("Registration received with no data");
+      return res.status(400).send("Registration received with no data");
+    }
 
-		try {
-			await addSale(results);
-			await writeToSheet(results, "Sales");
-			await ntfy({ route: "/sale", payload: results });
-			res.status(200).json({ message: "Sale added" });
-		} catch (error) {
-			console.error("Error in /sale:", error);
-			res.status(500).send(internalServerError);
-		}
-	});
+    try {
+      await addSale(results);
+      if (results.registrationType === registration) addCustomer(results);
+      await writeToSheet(results, "Sales");
+      await ntfy({ route: "/sale", payload: results });
+      res.status(200).json({ message: "Sale added" });
+    } catch (error) {
+      console.error("Error in /sale:", error);
+      res.status(500).send(internalServerError);
+    }
+  });
 
 export { router as saleRouter };

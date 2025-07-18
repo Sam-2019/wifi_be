85acc3a0236484501d8956fce197841e2d2df7c5
Mckenzie <rduser_1@outlookcom>
2025-07-18 14:14:41 +0000

    redundant code

--- src/services/alerts.js
+++ src/services/alerts.js
@@ -6,45 +6,13 @@ const topic = ntfyTopic;
 
 const setupAlert = (title, message) => {
   return {
-    "topic": topic,
-    "title": title,
+    topic: topic,
+    title: title,
     // "tags": tags,
-    "priority": priority,
-    "message": JSON.stringify(message),
+    priority: priority,
+    message: JSON.stringify(message),
   };
-}
-
-const saleAlert = (payload) => {
-  return setupAlert("New Sale", payload);
-}
-
-const registrationAlert = (payload) => {
-  return setupAlert("Registration", payload);
-}
-
-const pendingAlert = (payload) => {
-  return setupAlert("Pending Registration", payload);
-}
-
-const failedAlert = (payload) => {
-  return setupAlert("Failed Registration", payload);
-}
-
-const customerAlert = (payload) => {
-  return setupAlert("New Customer", payload);
-}
-
-const callbackAlert = (payload) => {
-  return setupAlert("Payment Callback", payload);
-}
-
-const statusCheckAlert = (payload) => {
-  return setupAlert("Payment Status Check", payload);
-}
-
-const syncAlert = (payload) => {
-  return setupAlert("Data Sync", payload);
-}
+};
 
 export const ntfy = async ({ payload, route }) => {
   if (!ntfyUri || !ntfyTopic || !ntfyAuthorization) {
@@ -69,40 +37,16 @@ export const ntfy = async ({ payload, route }) => {
     subscriptionPlan: payload?.subscriptionPlan,
     clientReference: payload?.clientReference,
     userName: payload?.credentials?.username,
-    registrationType: payload?.registrationType
-  }
-
-
-  const getPayload = (route) => {
-    switch (route) {
-      case '/sale':
-        return saleAlert(getInfo);
-      case '/payment/sync':
-        return syncAlert(getInfo);
-      case '/failed-registration':
-        return failedAlert(getInfo);
-      case '/pending-registration':
-        return pendingAlert(getInfo);
-      case '/customer':
-        return customerAlert(getInfo);
-      case '/payment/callback':
-        return callbackAlert(getInfo);
-      case '/payment/status':
-        return statusCheckAlert(getInfo);
-      case '/registration':
-        return registrationAlert(getInfo);
-      default:
-        console.error("Invalid alert type provided.");
-        return null;
-    }
+    registrationType: payload?.registrationType,
   };
+
+  const alert = setupAlert(route, getInfo);
 
   await fetch(ntfyUri, {
     method: "POST",
     headers: {
-      'Authorization': `Bearer ${ntfyAuthorization}`
+      Authorization: `Bearer ${ntfyAuthorization}`,
     },
-    body: JSON.stringify(getPayload(route)),
-  })
-
+    body: JSON.stringify(alert),
+  });
 };

68fb2f7f657bb7861a6995f5e0d9b5f4f3f57758
Sam-2019 <samtmartey@gmail.com>
2025-07-23 16:54:37 +0000

    update scheduler

--- src/services/jobs/index.js
+++ src/services/jobs/index.js
@@ -9,7 +9,7 @@ async function startBree() {
     root: path.resolve("./src/services/jobs"),
     jobs: [
       {
-        closeWorkerAfterMs: 10,
+        closeWorkerAfterMs: 10000,
         name: "provisionAccount",
         timeout: false,
         interval: "2m",

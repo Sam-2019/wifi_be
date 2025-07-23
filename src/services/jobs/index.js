76baf2c79879f79c0e86f5c50ba59241fa2fab76
Sam-2019 <samtmartey@gmail.com>
2025-07-23 13:10:18 +0000

    update scheduler

--- src/services/jobs/index.js
+++ src/services/jobs/index.js
@@ -5,6 +5,7 @@ import { __dirname } from "../../config/constants.js";
 
 async function startBree() {
   const bree = new Bree({
+    removeCompleted: true,
     root: path.resolve("./src/services/jobs"),
     jobs: [
       {

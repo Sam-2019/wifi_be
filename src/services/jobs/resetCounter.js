6c14d86585cfb2865288aa9a6ae4982d107dac18
Sam-2019 <samtmartey@gmail.com>
2025-07-24 20:28:42 +0000

    resetCounter

--- src/services/jobs/resetCounter.js
+++ src/services/jobs/resetCounter.js
@@ -1,6 +1,7 @@
 import { ntfy } from "../alerts/ntfy.js";
 import { resetCounter } from "../mikrotik/index.js";
 import { todaySales } from "../db/repository/sale.js";
+import { dataPlans } from "../../config/constants.js";
 import { connectDB, disconnectDB } from "../db/index.js";
 
 const resetCounter = async () => {
@@ -14,8 +15,31 @@ const resetCounter = async () => {
       await ntfy({ route: "/noSale", payload: noSale });
       return;
     }
+
+    const customer = sale[0];
+    const userName = customer?.credentials.userName;
+    const userInfo = await getUser(userName);
+
+    if (!userInfo) {
+      const message = `☑️ User not found`;
+      console.log(message);
+      return;
+    }
+
+    const userID = userInfo?.id;
+    const userUptime = userInfo?.uptime;
+
+    if (userUptime === dataPlans.DAILY.uptime) {
+      await resetCounter(userID);
+      const message = `✅ Reset Counter: ${customer.fullName} - ${userData.name}`;
+      console.log(message);
+      return;
+    }
+
+    
   } catch (error) {
-    console.error(error);
+    const message = `❌ Error: ${error}`;
+    console.error(message);
   } finally {
     await disconnectDB();
     console.log(`[${new Date().toISOString()}] resetCounter job finished.`);

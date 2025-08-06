8d109424237bb6f56ad6ebb435ed0a9539774f30
Sam-2019 <samtmartey@gmail.com>
2025-08-06 12:57:01 +0000

    remove regID

--- src/services/db/modelsXschema/pending_registration.js
+++ src/services/db/modelsXschema/pending_registration.js
@@ -2,7 +2,6 @@ import { model, Schema } from "mongoose";
 
 const dataSchema = new Schema(
   {
-    regID: { type: String },
     fullName: { type: String },
     phoneNumber: { type: String },
     subscriptionPlan: { type: String },
